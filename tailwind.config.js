4e7b3415d4e4a52aafa3c925f60d998c19b9b7b8
alnomanbd <alnoman.cse@outlook.com>
2024-11-23 20:15:22 +0600

    navbar section done

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,8 @@ export default {
         'neutralSilver' : '#F5F7FA', 
         'neutralDGrey' : '#4D4D4D', 
         'brandPrimary' : '#4CAF4F', 
-        'neutralGrey' : '#717171',
+        'neutralGrey': '#717171',
+        'gray900': '#18191F'
       }
     },
   },
