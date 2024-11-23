4ae5b47e0f56a9edd92816e4e0a315ad3b9d6f56
alnomanbd <alnoman.cse@outlook.com>
2024-11-23 20:56:07 +0600

    home section done

---
+++ src/components/Services.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Services = () => {
+    return (
+        <div className='md:px-14 px-4 '>
+            <div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Services
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
+const flowbite = require("flowbite-react/tailwind");
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    flowbite.content()
   ],
   theme: {
     extend: {
@@ -15,5 +17,7 @@ export default {
       }
     },
   },
-  plugins: [],
+  plugins: [
+    flowbite.plugin()
+  ],
 }
