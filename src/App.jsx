2e1e143b587793a65bb2d2ccad6007ebdb32b37f
alnomanbd <alnoman.cse@outlook.com>
2024-11-24 13:32:57 +0600

    partial project done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import './App.css'
+import About from './components/About'
+import Blog from './components/Blog'
 import Home from './components/Home'
 import Navbar from './components/Navbar'
+import Newsletter from './components/Newsletter'
+import Products from './components/Products'
 import Services from './components/Services'
+import Footer from './components/Footer'
+
 export default function App() {
   return (
     <>
       <Navbar />
       <Home />
-      <Services/>
+      <Services />
+      <About />
+      <Products />
+      <Blog />
+      <Newsletter />
+      <Footer/>
     </>
   )
 }
